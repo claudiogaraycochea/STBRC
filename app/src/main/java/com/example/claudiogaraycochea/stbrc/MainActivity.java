2b10930a1d6cf7022cb14d8e3684aa54e96c9c38
cgaraycochea <computadoraweb@gmail.com>
2015-04-22 13:15:20 -0300

    Browser

--- app/src/main/java/com/example/claudiogaraycochea/stbrc/MainActivity.java
+++ app/src/main/java/com/example/claudiogaraycochea/stbrc/MainActivity.java
@@ -4,16 +4,79 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.webkit.WebView;
+import android.webkit.WebSettings;
+import android.widget.Button;
+import android.view.View.OnClickListener;
+import android.view.View;
+import android.widget.Toast;
+import android.content.Context;
+import android.app.AlertDialog;
 
 public class MainActivity extends ActionBarActivity {
 
+    private WebView mWebView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mWebView = (WebView) findViewById(R.id.activity_main_webview);
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        mWebView.loadUrl("file:///android_asset/index.html");
+
+        Button toastButton = (Button) this.findViewById(R.id.toastButton);
+        toastButton.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                simpleFunction();
+                String data = "json{'name':'Claudio','age':'33'}";
+                mWebView.loadUrl("javascript:showTest(\""+data+"\")");
+            }
+        });
+
+        final MyJavaScriptInterface myJavaScriptInterface
+                = new MyJavaScriptInterface(this);
+        mWebView.addJavascriptInterface(myJavaScriptInterface, "AndroidFunction");
+
     }
 
+    public void simpleFunction(){
+        Toast.makeText(getApplicationContext(), "This is a plain toast.", Toast.LENGTH_SHORT).show();
+    }
+
+    public void openButton(View view){
+        Toast.makeText(getApplicationContext(), "Executing function open button.", Toast.LENGTH_SHORT).show();
+        mWebView.loadUrl("file:///android_asset/index.html");
+    }
+
+    public void openURL(String url){
+
+        mWebView.loadUrl("file:///android_asset/"+url);
+    }
+
+    public class MyJavaScriptInterface {
+        Context mContext;
+
+        MyJavaScriptInterface(Context c) {
+            mContext = c;
+        }
+
+        public void showToast(String toast){
+            Toast.makeText(mContext, toast, Toast.LENGTH_SHORT).show();
+        }
+
+        public void openAndroidDialog(){
+            AlertDialog.Builder myDialog
+                    = new AlertDialog.Builder(MainActivity.this);
+            myDialog.setTitle("DANGER!");
+            myDialog.setMessage("You can do what you want!");
+            myDialog.setPositiveButton("ON", null);
+            myDialog.show();
+        }
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -27,13 +90,18 @@ public class MainActivity extends ActionBarActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+            case R.id.menu_connection:
+                simpleFunction();
+                return true;
+            case R.id.menu_tv:
+                openURL("index_tv.html");
+                return true;
+            case R.id.menu_video:
+                openURL("index_video.html");
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
